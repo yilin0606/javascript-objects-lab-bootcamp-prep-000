841cfcf254a5d08cd7db8b409f2e48660d7669bd
Yilin Freya Gao <thisisgyl@gmail.com>
2019-10-12 21:19:48 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,7 @@
+//describe('Objects', function() {
+  it('defines a `recipes` object', function() {
+    expect(typeof recipes).toEqual('object')
+  })
+function Objects(recipes,object){
+  return 
+}
