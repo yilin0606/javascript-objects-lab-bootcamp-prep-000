839ca3468a5a3056d46fbbccef360ad110590b9f
Yilin Freya Gao <thisisgyl@gmail.com>
2019-10-12 21:26:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 var recipes={};
 
+describe('updateObjectWithKeyAndValue(object, key, value)', function() {
+    it('returns an object with the orignal key value pairs and the new key value pair', function() {
+      var obj = { prop: 1 }
+
+      expect(updateObjectWithKeyAndValue(obj, 'prop2', 2)).toMatch({
+        prop: 1,
+        prop2: 2
+      })
+    })
+
+    it('it does not modify the original object, but rather returns a clone with the new data', function() {
+      var obj = { prop: 1 }
+
+      updateObjectWithKeyAndValue(obj, 'prop2', 2)
+
+      expect(obj['prop2']).toBe(undefined)
+    })
+  })
+  
+  function updateObjectWithKeyAndValue(object, key, value){
+    return 
+  }
+  
+  
+  
+  
+  
+  
