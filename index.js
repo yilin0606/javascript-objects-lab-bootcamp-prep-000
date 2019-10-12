83e9f859a39afbcf716a93c1da6f3ee206339023
Yilin Freya Gao <thisisgyl@gmail.com>
2019-10-12 21:48:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,22 +12,7 @@ var recipes={};
   
   
   function deleteFromObjectByKey(object, key){
-    return 
+    delete object[key];
+    return object;
   }
-  
-  
-   describe('deleteFromObjectByKey(object, key)', function() {
-    it('deletes `key` from a clone of object and returns the new object (it is non-destructive)', function() {
-      var obj = { prop: 1 }
-      var newObj = deleteFromObjectByKey(obj, 'prop')
-
-      expect(newObj['prop']).toBe(undefined)
-    })
-
-    it('does not modify the original object (it is non-destructive)', function() {
-      var obj = { prop: 1 }
-
-      deleteFromObjectByKey(obj, 'prop')
-      expect(obj['prop']).toBe(1)
-    })
-  })
+ 
