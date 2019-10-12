d5495c52b873ce0b40cbdea8689773726b029e25
Yilin Freya Gao <thisisgyl@gmail.com>
2019-10-12 21:38:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,8 +20,7 @@ describe('updateObjectWithKeyAndValue(object, key, value)', function() {
   })
   
   function updateObjectWithKeyAndValue(object, key, value){
-    object[key]=value;
-    return object 
+   return Object.assign({},object,{[key]:value});
   }
   
   
